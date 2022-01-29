a7e36800eb7ca15f47fda5d7b63c78e853b97c67
TOHFU <87093hayato@gmail.com>
2022-01-29 12:16:45 +0900

    add button component

---
+++ src/components/common/Button.tsx
@@ -0,0 +1,48 @@
+import { ButtonHTMLAttributes, DetailedHTMLProps } from 'react';
+import styles from './Button.module.scss';
+
+export type ButtonType = 'button' | 'submit';
+export type ArrowType = 'next' | 'prev';
+
+type Props = {
+  type?: ButtonType;
+  label?: string;
+  className?: string;
+  disabled?: boolean;
+  arrow?: ArrowType;
+  round?: boolean;
+  onClick?: () => void;
+} & DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>;
+
+const Button: React.FC<Props> = ({
+  type = 'button',
+  label = '',
+  className = '',
+  disabled = false,
+  arrow,
+  round = false,
+  onClick,
+  ...attribute
+}) => {
+  const classNames: string[] = [
+    styles.Button,
+    arrow === 'next' ? styles.ButtonNext : '',
+    arrow === 'prev' ? styles.ButtonPrev : '',
+    round ? styles.ButtonRound : '',
+    className,
+  ];
+
+  return (
+    <button
+      type={type}
+      className={classNames.filter((item) => item.length).join(' ')}
+      onClick={onClick}
+      disabled={disabled}
+      {...attribute}
+    >
+      <span className={styles.ButtonLabel}>{label}</span>
+    </button>
+  );
+};
+
+export default Button;
